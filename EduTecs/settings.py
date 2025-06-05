d95e74a999d98ea7c680f6870b3b8c59bb861255
NicoDev-29 <almendra.garcia@tecsup.edu.pe>
2025-06-04 21:34:32 -0500

    ...

--- EduTecs/settings.py
+++ EduTecs/settings.py
@@ -132,7 +132,7 @@ JAZZMIN_SETTINGS = {
     "site_brand": "Edutecs",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "img/tecsup1.png",
+    "site_logo": "tecsup1.png",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
     "login_logo": None,
